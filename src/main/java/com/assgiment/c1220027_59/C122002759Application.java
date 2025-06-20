fa59fca3f13a2f5e6db16bf099bf67d76e57b0c3
Ismail Abdifadil <ismail.haabare@gmail.com>
2025-06-20 11:56:56 +0300

    Initial commit

---
+++ src/main/java/com/assgiment/c1220027_59/C122002759Application.java
@@ -0,0 +1,13 @@
+package com.assgiment.c1220027_59;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C122002759Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C122002759Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/assgiment/c1220027_59/C122002759ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assgiment.c1220027_59;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C122002759ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
